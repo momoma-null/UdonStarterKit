325fa1ab6c1280f25c58ab41588531b1f2ea6ec3
momoma <momoma.creation@gmail.com>
2022-07-11 15:07:08 +0900

    Update calibrating per 1sec

--- Udon/Runtime/CalibrationChair.cs
+++ Udon/Runtime/CalibrationChair.cs
@@ -11,34 +11,35 @@ namespace MomomaAssets.UdonStarterKit.Udon
         Transform _seatPosition = null;
 
         VRCPlayerApi _seatedPlayer;
-        int _progressCount;
 
         public override void Interact()
         {
             Networking.LocalPlayer.UseAttachedStation();
         }
 
-        void Update()
+        public void OneSecUpdate()
         {
-            if (_seatedPlayer != null && _seatPosition != null)
-            {
-                var playerPos = _seatedPlayer.GetPosition();
-                var leftLowerLegPos = _seatedPlayer.GetBonePosition(HumanBodyBones.LeftLowerLeg);
-                var rightLowerLegPos = _seatedPlayer.GetBonePosition(HumanBodyBones.RightLowerLeg);
-                var rightUpperLegPos = _seatedPlayer.GetBonePosition(HumanBodyBones.RightUpperLeg);
-                var height = (Mathf.Min(leftLowerLegPos.y, rightLowerLegPos.y) - playerPos.y) * -0.9f;
-                var depth = Vector3.Distance(rightLowerLegPos, rightUpperLegPos) * -0.6f;
-                _seatPosition.localPosition = new Vector3(0, height, depth);
-                ++_progressCount;
-                if (_progressCount > 30)
-                    _seatedPlayer = null;
-            }
+            if (_seatedPlayer == null)
+                return;
+            var playerPos = _seatedPlayer.GetPosition();
+            var leftLowerLegPos = _seatedPlayer.GetBonePosition(HumanBodyBones.LeftLowerLeg);
+            var rightLowerLegPos = _seatedPlayer.GetBonePosition(HumanBodyBones.RightLowerLeg);
+            var rightUpperLegPos = _seatedPlayer.GetBonePosition(HumanBodyBones.RightUpperLeg);
+            var height = (Mathf.Min(leftLowerLegPos.y, rightLowerLegPos.y) - playerPos.y) * -0.9f;
+            var depth = Vector3.Distance(rightLowerLegPos, rightUpperLegPos) * -0.6f;
+            _seatPosition.localPosition = new Vector3(0, height, depth);
+            SendCustomEventDelayedSeconds(nameof(OneSecUpdate), 1f);
         }
 
         public override void OnStationEntered(VRCPlayerApi player)
         {
             _seatedPlayer = player;
-            _progressCount = 0;
+            OneSecUpdate();
+        }
+
+        public override void OnStationExited(VRCPlayerApi player)
+        {
+            _seatedPlayer = null;
         }
     }
 }
