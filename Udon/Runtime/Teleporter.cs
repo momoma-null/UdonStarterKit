76b64bc75257d04121dc59008ae2baef6af13436
momoma <momoma.creation@gmail.com>
2022-07-23 13:15:12 +0900

    Add Teleporter prefabs

---
+++ Udon/Runtime/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace MomomaAssets.UdonStarterKit.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public sealed class Teleporter : UdonSharpBehaviour
+    {
+        [SerializeField]
+        Transform teleportPoint = null;
+        [SerializeField]
+        VRC_SceneDescriptor.SpawnOrientation orientation = VRC_SceneDescriptor.SpawnOrientation.Default;
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+                player.TeleportTo(teleportPoint.position, teleportPoint.rotation, orientation, false);
+        }
+    }
+}
