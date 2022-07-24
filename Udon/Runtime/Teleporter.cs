044f46cf026c3b1bc10f70ca009b0bff9fba63b3
momoma <momoma.creation@gmail.com>
2022-07-24 10:34:25 +0900

    Add teleport sound

--- Udon/Runtime/Teleporter.cs
+++ Udon/Runtime/Teleporter.cs
@@ -11,11 +11,19 @@ namespace MomomaAssets.UdonStarterKit.Udon
         Transform teleportPoint = null;
         [SerializeField]
         VRC_SceneDescriptor.SpawnOrientation orientation = VRC_SceneDescriptor.SpawnOrientation.Default;
+        [SerializeField]
+        AudioClip teleportAudio = null;
+        [SerializeField, HideInInspector]
+        AudioSource audioSource = null;
 
         public override void OnPlayerTriggerEnter(VRCPlayerApi player)
         {
             if (player.isLocal)
+            {
                 player.TeleportTo(teleportPoint.position, teleportPoint.rotation, orientation, false);
+                if (audioSource != null && teleportAudio != null)
+                    audioSource.PlayOneShot(teleportAudio);
+            }
         }
     }
 }
