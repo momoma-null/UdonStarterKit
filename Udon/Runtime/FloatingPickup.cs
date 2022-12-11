e03e58d8f961221825276ce9af0f96b45c2845e7
momoma <momoma.creation@gmail.com>
2022-12-11 22:25:33 +0900

    Add Floating Pickup

---
+++ Udon/Runtime/FloatingPickup.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace MomomaAssets.UdonStarterKit.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public sealed class FloatingPickup : UdonSharpBehaviour
+    {
+        [SerializeField]
+        PositionConstraint constraint;
+        [SerializeField]
+        Transform puller;
+        [SerializeField]
+        Transform anchor;
+        [SerializeField]
+        float offset = 0.05f;
+
+        public override void OnPickup()
+        {
+            constraint.constraintActive = false;
+        }
+
+        public override void OnDrop()
+        {
+            puller.position += Vector3.down * offset;
+            anchor.position = constraint.transform.position;
+            constraint.constraintActive = true;
+        }
+    }
+}
